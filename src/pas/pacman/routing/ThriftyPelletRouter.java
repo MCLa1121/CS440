202e25f8f9282b8311cc6f78c5172b714b6c8194
Battleoo <2993567797qq@gmail.com>
2026-02-22 02:14:54 -0500

    comment

--- src/pas/pacman/routing/ThriftyPelletRouter.java
+++ src/pas/pacman/routing/ThriftyPelletRouter.java
@@ -64,6 +64,12 @@ public class ThriftyPelletRouter
                               final ExtraParams params)
     {
         // TODO: implement me!
+        // function heuristic(node) =
+        // dx = abs(node.x - goal.x)
+        // dy = abs(node.y - goal.y)
+        // return D * (dx + dy)
+
+
         return 0f;
     }
 
@@ -71,6 +77,19 @@ public class ThriftyPelletRouter
     public Path<PelletVertex> graphSearch(final GameView game) 
     {
         // TODO: implement me!
+        // frontier = PriorityQueue()
+        // frontier.put ( start , 0)
+        // came_from = dict()
+        // cost_so_far = dict()
+        // came_from [ start ] = None
+        // cost_so_far [ start ] = 0 while not frontier.empty ():
+        //    current = frontier.get () if current == goal :
+        //       break for next in graph.neighbors( current ):
+        //       new_cost = cost_so_far [ current ] + graph.cost( current , next )
+        //       if next not in cost_so_far or new_cost < cost_so_far [ next ]:
+        //          cost_so_far [ next ] = new_cost 
+        //         priority = new_cost + heuristic( goal , next ) frontier.put ( next , priority)
+        //          came_from [ next ] = current
         return null;
     }
 
