0c0767ff71be194fc21b2f4eec160b577c35d0d0
Battleoo <2993567797qq@gmail.com>
2026-02-22 21:48:47 -0500

    Implementing getoutgoingNeigh: Default neigbour; current location of pacman and all the reamining pellet

--- src/pas/pacman/routing/ThriftyPelletRouter.java
+++ src/pas/pacman/routing/ThriftyPelletRouter.java
@@ -1,13 +1,15 @@
 package src.pas.pacman.routing;
 
 
+import java.util.ArrayList;
 // SYSTEM IMPORTS
 import java.util.Collection;
-
+import java.util.LinkedList;
 
 // JAVA PROJECT IMPORTS
 import edu.bu.pas.pacman.game.Action;
 import edu.bu.pas.pacman.game.Game.GameView;
+import edu.bu.pas.pacman.game.Tile;
 import edu.bu.pas.pacman.graph.Path;
 import edu.bu.pas.pacman.graph.PelletGraph.PelletVertex;
 import edu.bu.pas.pacman.routing.PelletRouter;
@@ -46,7 +48,58 @@ public class ThriftyPelletRouter
                                                          final ExtraParams params)
     {
         // TODO: implement me!
-        return null;
+        //create a collection Note: Data type: Collection<PelletVertex>; using Arraylist to store the neighbour
+        Collection<PelletVertex> neighbour = new ArrayList<>(); 
+
+        //get current coordinate: create object current that gather info from game 
+        PelletVertex current = new PelletVertex(game);
+
+        // get pacman location
+        Coordinate current_pac_location = current.getPacmanCoordinate();
+
+        // get remaining pellets in the game and store all the information to linklist
+        LinkedList<Coordinate> current_remaining_pellet = new LinkedList<>(current.getRemainingPelletCoordinates());
+
+        //get all the neighbour coordinate 
+        //moving right 
+        int dx = x + 1; 
+        Coordinate right = new Coordinate(dx, y);
+        //checking the tile 
+        Tile move = game.getTile(right);
+        //check whether the new tile is wall or not, if it is not a wall
+        //add it in to the list 
+        if(!move.getState().equals(Tile.State.WALL)){
+            neighbour.add(right);
+        }
+        
+        //moving uo 
+        int dy = y + 1; 
+        Coordinate up = new Coordinate(x, dy);
+        Tile moveUP = game.getTile(up);
+        if(!moveUP.getState().equals(Tile.State.WALL)){
+            neighbour.add(up);
+        }
+
+        //moving left 
+        int L = x - 1;
+        Coordinate left = new Coordinate(L, y);
+        Tile moveL = game.getTile(left);
+        if(!moveL.getState().equals(Tile.State.WALL)){
+            neighbour.add(left);
+        }
+
+        //moving down 
+        int D = y - 1;
+        Coordinate down = new Coordinate(x, D);
+        Tile moveD = game.getTile(down);
+        if(!moveD.getState().equals(Tile.State.WALL)){
+            neighbour.add(down);
+        }
+        
+        
+        return neighbour;
+
+        // return null;
     }
 
     @Override
