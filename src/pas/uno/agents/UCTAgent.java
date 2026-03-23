ea976b80385e1b52df55b6f195bf375188ad62ab
Battleoo <2993567797qq@gmail.com>
2026-03-23 16:09:45 -0400

    Implementing proxy agent; Fixing other method which use proxy agent; add two argument in mcts constructor; Fixing get child; Fixing choiceToMove; Fixing rollout: delete rollout limitation; Fixing search method: init the proxy agent in search ; modigy the thinking time; delete the iteraton size limitation which now all depend on the thinking buget; still make the buget in half of the getmaxthinking time; if not timeout will happen;

--- src/pas/uno/agents/UCTAgent.java
+++ src/pas/uno/agents/UCTAgent.java
@@ -7,11 +7,11 @@ import edu.bu.pas.uno.Game;
 import edu.bu.pas.uno.Game.GameView;
 import edu.bu.pas.uno.Hand;
 import edu.bu.pas.uno.Hand.HandView;
-import edu.bu.pas.uno.agents.MCTSAgent;
 import edu.bu.pas.uno.enums.Color;
 import edu.bu.pas.uno.enums.Value;
 import edu.bu.pas.uno.moves.Move;
 import edu.bu.pas.uno.tree.Node;
+import edu.bu.pas.uno.agents.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,15 +25,42 @@ import java.util.Set;
 public class UCTAgent
     extends MCTSAgent
 {
+    // create a proxyAgentclass and create the proxyAgent constructor
+    private static class ProxyAgent extends Agent {
+        public ProxyAgent(int playerIdx, long maxThinkingTimeInMS) {
+            super(playerIdx, maxThinkingTimeInMS);
+        }
+
+        @Override
+        public Move chooseCardToPlay(Game.GameView game) {
+            return null; // never used
+        }
+
+        @Override
+        public Move maybePlayDrawnCard(Game.GameView game, int drawnCardIdx) {
+            return null; // never used
+        }
+    }
+    private ProxyAgent[] proxyAgents;
 
     public static class MCTSNode
         extends Node
     {
+        private final Agent[] simulationAgents;
+        private final Integer drawnCardIdx;
         public MCTSNode(final GameView game,
                         final int logicalPlayerIdx,
-                        final Node parent)
+                        final Node parent,
+                        final Agent[] simulationAgents,
+                        final Integer drawnCardIdx)
         {
             super(game, logicalPlayerIdx, parent);
+            this.simulationAgents = simulationAgents; 
+            this.drawnCardIdx = drawnCardIdx;
+        }
+
+        public Integer getDrawnCardIdx() {
+            return this.drawnCardIdx;
         }
 
         // getChild method: return a mctsnode
@@ -41,7 +68,7 @@ public class UCTAgent
         public Node getChild(final Move move)
         {
             // set up a simulation 
-            Game simulation = new Game(this.getGameView());
+            Game simulation = new Game(this.getGameView(), this.simulationAgents);
             // get the current player index
             int current_player_index = this.getLogicalPlayerIdx();
             // get current hand from the current player
@@ -50,18 +77,31 @@ public class UCTAgent
             // if the unrealoved draw is avalilble
             if (this.getNodeState() == Node.NodeState.NO_LEGAL_MOVES_UNRESOLVED_CARDS_PRESENT) {
                 // player need to draw the total
+                System.out.println("getChild unresolved");
+
                 simulation.drawTotal(current_hand,simulation.getUnresolvedCards().total());
+                
+                System.out.println("before resolveMove null");
+                
+                simulation.resolveMove(null);
+
+                System.out.println("after resolveMove null");
+            } else {
+
+                System.out.println("before resolveMove move");
+                // then we move to reslovedmove null
+                // resolved move apply one time (if use a while loop here the agent will do noting)
+                simulation.resolveMove(move);
+                System.out.println("after resolveMove move");
             }
-
-            // then we move to reslovedmove null
-            // resolved move apply one time (if use a while loop here the agent will do noting)
-            simulation.resolveMove(move);
-
             // get the next state of the simulatoin (the current player index)
             int next_state = simulation.getPlayerOrder().getCurrentLogicalPlayerIdx();
 
+            // get the next player id
+            int next_player_id = simulation.getPlayerOrder().getAgentIdx(next_state);
             // return a mctsnode after we get the next state 
-            MCTSNode mctsnode = new MCTSNode(simulation.getView(next_state), next_state, this);
+            MCTSNode mctsnode = new MCTSNode(simulation.getView(next_player_id), next_state, this, this.simulationAgents,null);
+            System.out.println("getChild end");
             return mctsnode;
         }
     }
@@ -72,12 +112,15 @@ public class UCTAgent
         super(playerIdx, maxThinkingTimeInMS);
     }
 
+
+
     // ---------------------------- PRIVATE HELPER -------------------------------------------------------------------------------------------------------------------------
     // private helper -------------- help to make the choice to move
     private Move choiceToMove(Node node, int index) {
+        int player_index = node.getLogicalPlayerIdx();
         // if can legally move
         if (node.getNodeState() == Node.NodeState.HAS_LEGAL_MOVES) {
-            return wildMove(node.getGameView(),node.getLogicalPlayerIdx(), node.getOrderedLegalMoves().get(index));
+            return wildMove(node.getGameView(),player_index, node.getOrderedLegalMoves().get(index));
         }
 
         // if no leagal move and no unresloved card persent reutnr null which here we need to draw card
@@ -88,13 +131,17 @@ public class UCTAgent
         // if no legal move , and may play the drawn card 
         if (node.getNodeState() == Node.NodeState.NO_LEGAL_MOVES_MAY_PLAY_DRAWN_CARD) {
             // if we can play the card
-            if (index == Node.NoLegalMovesIdxDefaults.DrawSingleCardIdxs.PLAY_CARD_MOVE_IDX) {
-                // -1 because we are in a index form
-                int draw_card_index = node.getGameView().getHandView(node.getLogicalPlayerIdx()).size() - 1;
-                return wildMove(node.getGameView(), node.getLogicalPlayerIdx(), draw_card_index);
-            }else{
-                return null; // keep the card if non of the move can be make
+            if (index == Node.NoLegalMovesIdxDefaults.DrawSingleCardIdxs.KEEP_CARD_MOVE_IDX) {
+                // return null if no legal move allowed
+                return null;
             }
+            Integer drawnCardIdx = ((MCTSNode) node).getDrawnCardIdx();
+
+            if (drawnCardIdx == null) {
+                return null; 
+            }
+
+            return wildMove(node.getGameView(), player_index, drawnCardIdx);
         }
         return null; // return null if we make the draw and do nothing
     }
@@ -102,23 +149,27 @@ public class UCTAgent
     // private helper ------------- help to make the choic to move if we have wild card
     private Move wildMove(GameView game, int player_index, int card_index) {
         Card card_in_hand = game.getHandView(player_index).getCard(card_index);
+        ProxyAgent proxyAgent = proxyAgents[player_index];
+
         if (card_in_hand.isWild()) {
-            return Move.createMove(this, card_index,Color.getRandomColor(getRandom()));
+            return Move.createMove(proxyAgent, card_index,Color.getRandomColor(getRandom()));
         }
-        return Move.createMove(this, card_index);
+        
+        return Move.createMove(proxyAgent, card_index);
     }
 
     // private helper -------------help to rollout
     private float rollout(GameView gameview) {
         // set a simulation game
-        System.out.println("start rollout");
-        Game simulation_game = new Game(gameview);
+        Game simulation_game = new Game(gameview,proxyAgents);
 
         // while the game is not over keep playing
         // bug: might be the simualation is too mluch make it smaller
-        int size = 0;
-        while (!simulation_game.isOver() && size < 200) {
-            size ++; 
+        // int size = 0;
+
+        // need to make rollout cheaper; add thread.currentThread().isInterrputed so we can stop the rollout if we are timeout in the search
+        while (!Thread.currentThread().isInterrupted() && !simulation_game.isOver()) {
+            // size ++; 
             // get the current player index, and get the current card in hand, and set move to null
             int current_player_index = simulation_game.getPlayerOrder().getCurrentLogicalPlayerIdx();
             Hand current_card_hand = simulation_game.getHand(current_player_index);
@@ -132,7 +183,7 @@ public class UCTAgent
                 // pick a random card base on the legal move we can make 
                 int Pick_random_card = legal_moves.get(this.getRandom().nextInt(legal_moves.size()));
                 // call wild move to return a move if we have wild card
-                move = wildMove(simulation_game.getView(current_player_index), current_player_index, Pick_random_card);
+                move = wildMove(simulation_game.getView(simulation_game.getPlayerOrder().getAgentIdx(current_player_index)), current_player_index, Pick_random_card);
             
             
             } else if (simulation_game.getUnresolvedCards().isEmpty()) {
@@ -144,9 +195,9 @@ public class UCTAgent
 
                 // if the drawn card are legal to pley
                 if (draw_card.canBePlayedAsDrawCard(simulation_game)) {
-                    move = wildMove(simulation_game.getView(current_player_index), current_player_index, draw_card_index);
+                    move = wildMove(simulation_game.getView(simulation_game.getPlayerOrder().getAgentIdx(current_player_index)), current_player_index, draw_card_index);
                 } else {
-                    move = null; // we keep the card taht we have drawn
+                    move = null; // make sur ewe end the turn whe call resolvedMove
                 }
             
             // otherwise we need to draw the card and add the total draw card to the with the unreaoved card that have in total
@@ -158,7 +209,7 @@ public class UCTAgent
             // if the move is null then call resovlved move to move to the next turn
             simulation_game.resolveMove(move);
         }
-        System.out.println("end rollout");
+        
         // ------------ Win OR Lose --------
         // if we have play all of the card in our hand (0 card in hand)
         if (simulation_game.getHand(this.getLogicalPlayerIdx()).size() == 0) {
@@ -218,8 +269,8 @@ public class UCTAgent
         // create an array list to store the best ucb index in there
         ArrayList<Integer> best_ucb_index = new ArrayList<>();
 
-        // totoal count of all visit time with all nodes's vists times
-        long total_count = node.getStateCount();
+        // totoal count of all visit time with all nodes's vists times use math.max to avoid using zero
+        long total_count = Math.max(1,node.getStateCount());
 
         // get the number of choice that we have
         int num_of_choice = getNumberOfChoices(node);
@@ -281,15 +332,51 @@ public class UCTAgent
     {
         // TODO: implement me!
         // for debuging
-        System.out.println("enter search");
+        // add a itration control because it continue to timeout
+        // int iterate_count = 0;
+        // debuge print statement
+        System.out.println("search start");
+
+        if (proxyAgents == null) {
+            int n = game.getNumPlayers();
+            proxyAgents = new ProxyAgent[n];
+        
+            for (int i = 0; i < n; i++) {
+                int actualPlayerIdx = game.getPlayerOrder().getAgentIdx(i);
+                proxyAgents[i] = new ProxyAgent(actualPlayerIdx, this.getMaxThinkingTimeInMS());
+                proxyAgents[i].setLogicalPlayerIdx(i);
+            }
+        }
+        
+        try {
         // Bug: getlogicalPlayerIdx(); change it to game.getplayerOrder().getCurrentLogicalPlayerIdx()
-        MCTSNode root_node = new MCTSNode(game, game.getPlayerOrder().getCurrentLogicalPlayerIdx(), null);
+        int player_root_index = game.getPlayerOrder().getCurrentLogicalPlayerIdx();
+
+        MCTSNode root_node = new MCTSNode(game, player_root_index, null, proxyAgents,drawnCardIdx);
+        
+        // if there is no legal move and no unrealoved draw and no drawn card
+        if (drawnCardIdx == null && game.getHandView(player_root_index).getLegalMoves(game).isEmpty() && game.getUnresolvedCards().isEmpty()) {
+            
+            // we wnat to choo keep card move idx to null so use set q method
+            root_node.setQCount(Node.NoLegalMovesIdxDefaults.DrawSingleCardIdxs.KEEP_CARD_MOVE_IDX, 1);
+            root_node.setQValueTotal(Node.NoLegalMovesIdxDefaults.DrawSingleCardIdxs.KEEP_CARD_MOVE_IDX, 0.0f);
+            
+            // debug print statement
+            System.out.println("early root return");
+
+            // reutn the roo node
+            return root_node; 
+        }
+        
         long Start_of_thinking_time = System.currentTimeMillis();
-        long budget = 20; 
+        long budget = Math.max(1, this.getMaxThinkingTimeInMS()/2); 
+
+
 
         //---------- while we still have budget to think keep loop running ----------
-        while (System.currentTimeMillis() - Start_of_thinking_time < budget) {
-
+        // add theread to check if my agent timeout 
+        while (!Thread.currentThread().isInterrupted() && System.currentTimeMillis() - Start_of_thinking_time < budget) {
+            // iterate_count++;
             Node current = root_node; 
 
             // -- for backpropageation --
@@ -297,9 +384,14 @@ public class UCTAgent
             ArrayList<Integer> move_path = new ArrayList<>();
             node_path.add(current);
 
+            // debug print
+            System.out.println("A");
+
             // ------------ selection ------------
             // if not a leaf node keep running
-            while (!current.isTerminal()) {
+            // also add a theard is interrupted here to check the timeout
+            // therad reference: https://stackoverflow.com/questions/11682955/why-use-thread-currentthread-isinterrupted-instead-of-isinterrupted
+            while (!Thread.currentThread().isInterrupted() && !current.isTerminal()) {
 
                 // set not visited as -1
                 int  not_visited = -1;
@@ -355,14 +447,33 @@ public class UCTAgent
                 }  
             }
 
+            // break if time out
+            if (Thread.currentThread().isInterrupted()) {
+                break;
+            }
+
+            // debug print
+            System.out.println("B");
+
             // ----------- Rollout --------------
             float rollout_value = rollout(current.getGameView());
+
+            // debug print
+            System.out.println("C");
+
             // ----------- Backpropagation ---------
             backpropagate(node_path, move_path, rollout_value);
         }
 
+        // for debugging (print statement) bug: the two print statement is not shown in the cmd
+
+
         // return root_node after we done
         return root_node;
+    } finally {
+        // System.out.println("iterations = " + iterate_count);
+        System.out.println("search end");
+    }
 
     }
 
