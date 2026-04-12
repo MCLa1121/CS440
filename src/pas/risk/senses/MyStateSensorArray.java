859f984819e9bea7e5ea4edc605ae702319ac37b
MCLa1121 <ericwu20011213@gmail.com>
2026-04-12 19:22:19 -0400

    risk project

---
+++ src/pas/risk/senses/MyStateSensorArray.java
@@ -0,0 +1,33 @@
+package pas.risk.senses;
+
+
+// SYSTEM IMPORTS
+import edu.bu.jmat.Matrix;
+
+import edu.bu.pas.risk.GameView;
+import edu.bu.pas.risk.agent.senses.StateSensorArray;
+
+
+// JAVA PROJECT IMPORTS
+
+
+/**
+ * A suite of sensors to convert a {@link GameView} into a feature vector (must be a row-vector)
+ */ 
+public class MyStateSensorArray
+    extends StateSensorArray
+{
+    public static final int NUM_FEATURES = 15;
+
+    public MyStateSensorArray(final int agentId)
+    {
+        super(agentId);
+    }
+
+    public Matrix getSensorValues(final GameView state)
+    {
+        return Matrix.randn(1, NUM_FEATURES); // row vector
+    }
+
+}
+
