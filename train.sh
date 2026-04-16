c44ce449cd89999bf8a8ebe72876435be7afa3bf
Battleoo <2993567797qq@gmail.com>
2026-04-15 23:39:06 -0400

    upload train.sh

---
+++ train.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#$ -P cs440
+#$ -l h_rt=48:00:00
+#$ -l mem_per_core=4G
+#$ -pe omp 2
+#$ -j y
+#$ -m bea
+#$ -o /projectnb/cs440/students/jason01/train_log.log
+
+module load java/21
+
+cd /projectnb/cs440/students/jason01
+
+mkdir -p params
+
+javac -cp "./lib/*:." @risk.srcs
+
+java -cp "./lib/*:./src:." edu.bu.pas.risk.SequentialTrain pas.risk.agent.RiskQAgent pas.risk.agent.RiskQAgent static -x 5000 -t 5 -v 1 -u 50 -n 1.0E-4 -d adam -g 0.99 -o /projectnb/cs440/students/jason01/params/qFunction | tee /projectnb/cs440/students/jason01/my_logfile.log
