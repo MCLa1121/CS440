f383c5363ffafaed61b17d0988fcce3fc9d9cfc3
Battleoo <2993567797qq@gmail.com>
2026-04-17 01:32:06 -0400

    uploading train.sh 3 player; self player

--- train.sh
+++ train.sh
@@ -15,4 +15,4 @@ mkdir -p params
 
 javac -cp "./lib/*:." @risk.srcs
 
-java -cp "./lib/*:./src:." edu.bu.pas.risk.SequentialTrain pas.risk.agent.RiskQAgent pas.risk.agent.RiskQAgent static -x 5000 -t 5 -v 1 -u 50 -n 1.0E-4 -d adam -g 0.99 -o /projectnb/cs440/students/jason01/params/qFunction | tee /projectnb/cs440/students/jason01/my_logfile.log
+java -cp "./lib/*:./src:." edu.bu.pas.risk.SequentialTrain pas.risk.agent.RiskQAgent pas.risk.agent.RiskQAgent pas.risk.agent.RiskQAgent -x 5000 -t 5 -v 5 -u 50 -n 1.0E-4 -d adam -g 0.99 -i /projectnb/cs440/students/jason01/params/qFunction248.model --outOffset 249 -o /projectnb/cs440/students/jason01/params/qFunction 2>/dev/null | tee /projectnb/cs440/students/jason01/my_logfile.log
